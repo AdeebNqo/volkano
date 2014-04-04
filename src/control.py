9e55a8473da3eac5bc8985f9ec5e6c04e6455af3
AdeebNqo <adeebnqo@gmail.com>
2014-04-04 19:51:50 +0200

    hub not responding

--- src/control.py
+++ src/control.py
@@ -15,16 +15,32 @@
 import socket
 class Controller(object):
 	sockt = None
+	hubname = None
+	nick = 'johndoe'
+	passwrd = 'password'
 	#
 	# Method for connecting to a dc hub
 	# uses the nmdc protocol as outlined in http://nmdc.sourceforge.net/NMDC.html
 	#
 	def connect(self,addr):
-		sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sockt.connect(addr)
+		self.sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sockt.connect(addr)
 		#executing the steps of the nmdc protocol to connect
-		lock = sockt.recv(1024)
-		print('lock is {}'.format(lock))
+		lock = self.sockt.recv(1024)
+		lockarray = lock.split('|')
+		lock = lockarray[0]
+		if (len(lockarray)>1):		
+			# Hub does not need authentication
+			# Size of lockarray should be two
+			hubnamevar = lockarray[1]
+			self.hubname = hubnamevar.replace('$HubName ','')
+			# Identifying username
+			self.sockt.sendall('$ValidateNick {}'.format(self.nick))
+			print('sent username...')
+			hubresponse = self.sockt.recv(1024)
+			print('received response...')
+			hubresponse = hubresponse.split('|')
+			print('hubresponse is {}'.format(hubresponse))
 if __name__=='__main__':
 	controller = Controller()
 	addr = ('127.0.0.1',1200)
