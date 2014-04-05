138a49c9befcb8b7d42a63a390051518254eb040
AdeebNqo <adeebnqo@gmail.com>
2014-04-05 20:01:13 +0200

    need to try working with adc protocol as i cannot verify nmdc

--- src/control.py
+++ src/control.py
@@ -39,67 +39,76 @@ class Controller(object):
 	retries = 0
 	def connect(self,host,port):
 		try:
+			print('trying to connect...')
 			self.sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			self.sockt.settimeout(60)
 			self.sockt.connect((host,port))
-		except socket.timeout:
-			retries = retries+1
-			if retries<3:
+		except (socket.timeout,socket.error) as err:
+			self.retries = self.retries+1
+			if self.retries<3:
 				self.connect(host,port+1)
-		print('socket has been established!')
-		#Let hub speak first
-		data = self.sockt.recv(9000)
-		for item in data.split('|'):
-			if (item!=''):
-				whitespacepos = item.find(' ')
-				self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
-		#		
-		#Checking if hub requires key to be sent
-		#		
-		if (self.hubinfo['$Lock'].startswith('Sending_key_isn\'t_neccessary,_key_won\'t_be_checked')==False):
-			#Hub requires key
+		if (self.retries<3):
+			print('socket has been established!')
+			#Let hub speak first
+			data = self.sockt.recv(9000)
+			for item in data.split('|'):
+				if (item!=''):
+					whitespacepos = item.find(' ')
+					self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
+			#		
+			#Checking if hub requires key to be sent
+			#		
+			if (self.hubinfo['$Lock'].startswith('Sending_key_isn\'t_neccessary,_key_won\'t_be_checked')==False):
+				#Hub requires key
+				#
+				# Key will be computed using the info provided here http://nmdc.sourceforge.net/NMDC.html#_key
+				#
+				lockval = self.hubinfo['$Lock']
+				dot = lockval.find('.')
+				if (True if dot==-1 else False):
+					lock = lockval[lockval.find('$Lock')+1:]
+				else:
+					lock = lockval[lockval.find('$Lock')+1:dot]
+				lenlock = len(lock)
+				key = chr(ord(lock[0]) ^ ord(lock[lenlock-1]) ^ ord(lock[lenlock-2]) ^ 5)
+				for i in range(1,lenlock):
+					key = key+chr(ord(lock[i]) ^ ord(lock[i-1]))
+				finalkey = ''			
+				for i in range(len(key)):
+					finalkey = finalkey+chr(((ord(key[i])<<4) & 240) | ((ord(key[i])>>4) & 15))
+				self.sockt.sendall('$Key {}|'.format(finalkey))
+			self.sockt.sendall('$ValidateNick {}|'.format(self.nick))
 			#
-			# Key will be computed using the info provided here http://nmdc.sourceforge.net/NMDC.html#_key
-			#
-			lockval = self.hubinfo['$Lock']
-			dot = lockval.find('.')
-			if (True if dot==-1 else False):
-				lock = lockval[lockval.find('$Lock')+1:]
-			else:
-				lock = lockval[lockval.find('$Lock')+1:dot]
-			lenlock = len(lock)
-			key = chr(ord(lock[0]) ^ ord(lock[lenlock-1]) ^ ord(lock[lenlock-2]) ^ 5)
-			for i in range(1,lenlock):
-				key = key+chr(ord(lock[i]) ^ ord(lock[i-1]))
-			finalkey = ''			
-			for i in range(len(key)):
-				finalkey = finalkey+chr(((ord(key[i])<<4) & 240) | ((ord(key[i])>>4) & 15))
-			self.sockt.sendall('$Key {}|'.format(finalkey))
-		self.sockt.sendall('$ValidateNick {}|'.format(self.nick))
-		#
-		#Getting response from hub
-		response = self.sockt.recv(9000)
-		for item in response.split('|'):
-			if (item!=''):
-				whitespacepos = item.find(' ')
-				self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
-		if ('$GetPass' in self.hubinfo):
-			#
-			# if the hub requires authentication
-			self.sockt.sendall('$MyPass {}|'.format(self.password))
-		#Gained access to the hub
-		self.sockt.sendall('$Version 1,0091|')
-		self.sockt.sendall('$MyINFO {0} {1}$ $33.6kbps1${2}$5000$|'.format(self.nick,self.description,self.email))
-		response = self.sockt.recv(9000)
-		for item in response.split('|'):
-			if (item!=''):
-				whitespacepos = item.find(' ')
-				self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
-		print(self.hubinfo)
+			#Getting response from hub
+			response = self.sockt.recv(9000)
+			for item in response.split('|'):
+				if (item!=''):
+					whitespacepos = item.find(' ')
+					self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
+			if ('$GetPass' in self.hubinfo):
+				#
+				# if the hub requires authentication
+				self.sockt.sendall('$MyPass {}|'.format(self.password))
+			#Gained access to the hub
+			self.sockt.sendall('$Version 1,0091|')
+			self.sockt.sendall('$MyINFO {0} {1}$ $33.6kbps1${2}$5000$|'.format(self.nick,self.description,self.email))
+			response = self.sockt.recv(9000)
+			for item in response.split('|'):
+				if (item!=''):
+					whitespacepos = item.find(' ')
+					self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
+			print(self.hubinfo)
+			print('Now getting file lists...')
+			self.getfiles()
+		else:
+			raise Exception('Connection failed.')
 	#
-	#  
+	# get files  
 	#
-
+	def getfiles(self):
+		self.sockt.sendall('$GetNickList|')
+		response = self.sockt.recv(9000)
+		print(response)
 #
 # Utill method for retrieving ip from domain
 #
@@ -107,4 +116,4 @@ def getip(domain):
 	return socket.gethostbyname_ex(domain)[2][0]
 if __name__=='__main__':
 	controller = Controller()
-	controller.connect('127.0.0.1',1200)
+	controller.connect('127.0.0.1',411)
