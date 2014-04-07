bba16800db167eda832520143e1549e96a0294a1
AdeebNqo <adeebnqo@gmail.com>
2014-04-07 13:13:04 +0300

    reverted back to version that can connect

--- src/control.py
+++ src/control.py
@@ -13,7 +13,6 @@
 # Controller for the volkano, the nmdc protocol video stream application
 #
 import socket
-import select
 
 class Controller(object):
 	sockt = None
@@ -23,46 +22,11 @@ class Controller(object):
 	hubinfo = {}
 	#
 	# User credentials
-	nick = 'XV_yeah'
+	nick = 'sumarairiver'
 	password = 'default'
 	description = 'here lies dragons'
 	email = 'sumarai@testword.ru'
 
-	#
-	#Method for receiving data from hub
-	def recv(self):
-		data = ''
-		while True:
-			try:
-				ready = select.select([self.sockt],[],[],30)
-				if (ready[0]):
-					response = self.sockt.recv(1024)
-					print('response is {}'.format(response))
-					data = data+response
-					if not response:
-						break
-			except socket.timeout:
-				break
-		return data
-	#
-	#Method for receiving data from specific socket
-	def recv2(self,socket):
-		data = '';
-		while True:
-			try:
-				ready = select.select([self.sockt],[],[],30)
-				if (ready[0]):
-					response = socket.recv(1024)
-					print('response is {}'.format(response))
-					data = data+response
-					if not response:
-						break
-			except timeout:
-				break
-		return data
-	
-	#
-	# The following methods apply the xor function for strings
 	def xor(self,s1,s2):
 		return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
 	def xor(self,s1,num1):
@@ -77,9 +41,8 @@ class Controller(object):
 		try:
 			print('trying to connect...')
 			self.sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sockt.settimeout(60)
 			self.sockt.connect((host,port))
-			self.sockt.setblocking(0)
-			print('raw socket connected...')
 		except (socket.timeout,socket.error) as err:
 			self.retries = self.retries+1
 			if self.retries<3:
@@ -88,7 +51,7 @@ class Controller(object):
 			print('socket has been established!')
 			try:
 				#Let hub speak first
-				data = self.recv()
+				data = self.sockt.recv(9000)
 				for item in data.split('|'):
 					if (item!=''):
 						whitespacepos = item.find(' ')
@@ -118,16 +81,17 @@ class Controller(object):
 					finkey = ''
 					for i in range(len(key)):
 						finkey=finkey+chr(((ord(key[i]) & 0x0F) << 4) | ((ord(key[i]) & 0xF0) >> 4))
-					print('key is {0} and keylength is {1}'.format(finkey.encode('utf-8'), len(finkey)))
+					print('key is {0}, length:{1}'.format(finkey,len(finkey)))
 					self.sockt.sendall('$Key {}|'.format(finkey))
-					print('done sending key...')
-				print('abt to send nick...')
 				self.sockt.sendall('$ValidateNick {}|'.format(self.nick))
-				print('done sending nick and key to hub...')
 				#
 				#Getting response from hub
-				response = self.recv()
-				print('responded with {} after nick and key'.format(response))
+				self.sockt.setblocking(1)
+				self.sockt.settimeout(30)
+				while True:
+					response = self.sockt.recv(1024)
+					if ((not response)==False):
+						break
 				for item in response.split('|'):
 					if (item!=''):
 						whitespacepos = item.find(' ')
@@ -136,67 +100,37 @@ class Controller(object):
 					#
 					# if the hub requires authentication
 					self.sockt.sendall('$MyPass {}|'.format(self.password))
-				print('sent password if neccessary')
 				print('hubinfo is {}'.format(self.hubinfo))
-				print('sending vers and myinfo...')
-				print('$MyINFO $ALL {0} <StreamDC++ V	:2.42,M:A,H:2/0/0,S:32>$ $LAN(T3)0x31${1}$1234$|'.format(self.nick,self.email))
 				#Gained access to the hub
 				self.sockt.sendall('$Version 1,0091|')
-				self.sockt.sendall('$MyINFO $ALL {0} <StreamDC++ V:2.42,M:A,H:2/0/0,S:32>$ $LAN(T3)0x31${1}$1234$|'.format(self.nick,self.email))
-				print('sent myinfo to hub')
-				response = self.recv()
+				self.sockt.sendall('$MyINFO $ALL {0} <++ V:0.673,M:P,H:0/1/0,S:2>$ $LAN(T3)0x31${1}$1234$|'.format(self.nick,self.email))
+				response = self.sockt.recv(9000)
 				for item in response.split('|'):
 					if (item!=''):
 						whitespacepos = item.find(' ')
 						self.hubinfo[item[:whitespacepos]] = item[whitespacepos+1:]
 				print(self.hubinfo)
-				self.cachefiles()
+				print('Now getting file lists...')
+				self.getfiles()
 			except socket.timeout:
 				#
 				# We have to use adc instead of nmdc
-				print('hub does not support nmdc. Defaulting to adc...\n adc currently not supported yet.')
+				print('hub does not support nmdc. Defaulting to adc...')
 		else:
 			raise Exception('Connection failed.')
 	#
-	# Method for caching files
-	def cachefiles(self):
-		print('Now caching files...')
-		self.sockt.settimeout(50)
+	# get files  
+	#
+	def getfiles(self):
 		self.sockt.sendall('$GetNickList|')
-		data = self.recv()
-		data = data.split('|')
-		for item in data:
-			if item.startswith('$NickList'):
-				#getting all video files from the logged in users
-				users = item.replace('$NickList ','').split('$$')
-				for user in users:
-					if (user!='' or user!=self.nick):
-						#
-						#get file list of user and index it
-						self.sockt.sendall('$RevConnectToMe {0} {1}|'.format(self.nick,user))
-						response = self.recv()
-						for item in response.split('|'):
-							if item.startswith('$Search'):
-								vals = item.split(' ')
-								ipport = vals[1].split(':')
-								ip = ipport[0]
-								port = ipport[1]
-								searchstring = vals[2]
-								#
-								#handling 'other client'
-								#
-								print('the other client has addr, ip:{0} and port:{1}'.format(ip,port))
-								s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-								s.connect((ip,int(port)))
-								response = self.recv2(s)
-								print('client/server response is {}'.format(response))
-	
+		response = self.sockt.recv(9000)
+		print(response)
 #
-# Util method for retrieving ip from domain
+# Utill method for retrieving ip from domain
 #
 def getip(domain):
 	return socket.gethostbyname_ex(domain)[2][0]
 if __name__=='__main__':
-	print('creating controller...')
 	controller = Controller()
 	controller.connect('127.0.0.1',411)
+	x = input('quit?') # debug: for hanging onto the console
