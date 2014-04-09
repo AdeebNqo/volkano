f30ca075512401d8686f5f509d395555f943781f
AdeebNqo <adeebnqo@gmail.com>
2014-04-10 00:03:52 +0200

    adding java and gradle support

---
+++ src/main/java/control/Controller.java
@@ -0,0 +1,18 @@
+/*
+
+Zola Mahlaza (AdeebNqo)
+http:github.com/AdeebNqo
+
+“The problem with using C++ … is that there’s already a strong tendency in the language to require you to know everything before you can do anything.”
+
+- Larry Wall
+
+Class to controll access to dc hub and stream videos
+
+*/
+package control;
+public class Controller{
+	public Controller(){
+
+	}
+}
