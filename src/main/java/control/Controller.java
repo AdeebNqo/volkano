b6be7fdba37625e77a268e563dfd8dbfc45ebebd
AdeebNqo <adeebnqo@gmail.com>
2014-04-10 00:21:12 +0200

    added driver class for testing

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -11,8 +11,33 @@ Class to controll access to dc hub and stream videos
 
 */
 package control;
+
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
 public class Controller{
-	public Controller(){
+	
+	
+	private String IP;
+	private int port;
+
+	private Socket hubConnection;
+	private BufferedReader hubInput;
+	private PrintWriter hubOutput;
+
+	public Controller(String ip, int port){
+		IP = ip; port = port;
+	}
+	/*
+	
+	Method for connecting to a dc hub
+
+	*/
+	public void connect(){
+		hubConnection = new Socket(IP,port);
+		hubInput = new InputStreamReader(hubConnection.getInputStream());
+		hubOutput = new PrintWriter(hubConnection.getOutputStream());
+		System.err.println("TCP connection established.")
 
 	}
 }
---
+++ src/main/java/control/Driver.java
@@ -0,0 +1,12 @@
+/*
+
+Driver class for testing
+
+*/
+package control;
+public class Driver{
+	public static void main(String[] args){
+		Controller con = new Controller();
+		
+	}
+}
