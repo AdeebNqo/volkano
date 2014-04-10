d6fde1055367dde5ff770802ef72a629f9f91d37
AdeebNqo <adeebnqo@gmail.com>
2014-04-10 03:57:22 +0200

    can access other users. need to implement client-client handshake

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.util.LinkedList;
 
 public class Controller{
 	
@@ -34,6 +35,9 @@ public class Controller{
 
 	private String version = "1.00";
 
+	private LinkedList<String> OPlist;
+	private LinkedList<String> NickList;
+
 	public Controller(String ip, int port){
 		IP = ip; this.port = port;
 	}
@@ -43,6 +47,9 @@ public class Controller{
 
 	*/
 	public void connect() throws UnknownHostException,IOException{
+		OPlist  = new LinkedList<String>();
+		NickList = new LinkedList<String>();
+	
 		System.err.println("Attempting to connect to "+IP+":"+port);
 		hubConnection = new Socket(IP,port);
 		hubInput = hubConnection.getInputStream();
@@ -57,45 +64,71 @@ public class Controller{
 				String lock = values[1];
 				System.err.println("Lock value is "+lock);
 				int len = lock.length();
-				if (!lock.equals("ending_key_isn't_neccessary,_key_won't_be_checked.")){
-					//computing the key and sending...
-					String key  = ""+(char)(lock.charAt(0) ^ lock.charAt(len-1) ^ lock.charAt(len-2) ^ 5);
-					for (int i = 1; i < len; i++){
-						key += lock.charAt(i) ^ lock.charAt(i-1);
-					}
-					char[] newchars = new char[len];
-					for (int i = 0; i < len; i++){
-						char x = (char)((key.charAt(i) >> 4) & 0x0F0F0F0F);
-						char y = (char)((key.charAt(i) & 0x0F0F0F0F) << 4);
-						newchars[i] = (char)(x | y);
-					}
-					key = String.valueOf(newchars);
-					System.err.println("Sending "+key);
-					sendData("$Key "+key+"|");
+				
+				//computing the key and sending...
+				String key  = ""+(char)(lock.charAt(0) ^ lock.charAt(len-1) ^ lock.charAt(len-2) ^ 5);
+				for (int i = 1; i < len; i++){
+					key += lock.charAt(i) ^ lock.charAt(i-1);
 				}
-			}
-			//sending client's nick
-			sendData("$ValidateNick "+nick+"|");
-			String responseX = getResponse();
-			System.out.println("$validnick RESPONSE: "+responseX);
-
-			String[] itemsX = responseX.split("|");
-			for (String itemX:itemsX){
-				if (itemX.startsWith("$GetPass")){
-					//if hub requires password
-					sendData("$MyPass "+password+"|");
-					//retrieve hello client msg
-					String responseXY = getResponse();
-					System.err.println("RESPONSE: "+responseXY);
+				char[] newchars = new char[len];
+				for (int i = 0; i < len; i++){
+					char x = (char)((key.charAt(i) >> 4) & 0x0F0F0F0F);
+					char y = (char)((key.charAt(i) & 0x0F0F0F0F) << 4);
+					newchars[i] = (char)(x | y);
 				}
+				key = String.valueOf(newchars);
+				System.err.println("Sending "+key);
+				sendData("$Key "+key+"|");
 			}
-			//sending version and myinfo
-			sendData("$Version "+version+"|");
-			sendData("$MyINFO $ALL "+nick+" <++ V:0.673,M:P,H:0/1/0,S:2>$ $LAN(T3)0x31$test@test.com$1234$|");
 		}
-		System.out.println("Hub says "+response);
-		while(true){
-			int i = 0;
+
+		//sending client's nick
+		sendData("$ValidateNick "+nick+"|");
+		String responseX = getResponse();
+		System.out.println("$validnick RESPONSE: "+responseX);
+
+		String[] itemsX = responseX.split("|");
+		for (String itemX:itemsX){
+			if (itemX.startsWith("$GetPass")){
+				//if hub requires password
+				sendData("$MyPass "+password+"|");
+				//retrieve hello client msg
+				String responseXY = getResponse();
+				System.err.println("RESPONSE: "+responseXY);
+			}
+		}
+		//sending version and myinfo
+		sendData("$Version "+version+"|");
+		sendData("$MyINFO $ALL "+nick+" <++ V:0.673,M:P,H:0/1/0,S:2>$ $LAN(T3)0x31$test@test.com$1234$|");
+		sendData("$GetNickList|");
+		String listResponse = getResponse();
+		System.err.println("list response is "+listResponse);
+		String[] listItems = listResponse.split("\\|");
+		for (String listitem:listItems){
+			listitem = listitem.trim();
+			System.err.println(listitem);
+			if (listitem.startsWith("$OpList")){
+				String[] opusers = listitem.split(" ");
+				if (opusers.length>1){
+					for (int i=1; i<opusers.length; ++i){
+						OPlist.add(opusers[i]);				
+					}
+				}
+			}
+			else if (listitem.startsWith("$NickList")){
+				int pos = listitem.indexOf(' ');
+				String usersdoubledollar = listitem.substring(pos).trim();
+				String[] nicks = usersdoubledollar.split("\\$\\$");
+				for (String nick:nicks){
+					NickList.add(nick);
+				}
+			}
+		}
+		//getting the file lists of all connected users
+		for (String user:NickList){
+			if (!user.equals(nick) && !OPlist.contains(user)){
+				System.out.println("Get file list of "+user);
+			}
 		}
 	}
 	/*
