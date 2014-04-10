1c8fc3629b2d16af351870321bdc6ea6407dbdff
AdeebNqo <adeebnqo@gmail.com>
2014-04-10 13:00:07 +0200

    debugging file get

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -20,10 +20,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.LinkedList;
+import java.net.ServerSocket;
+import java.security.SecureRandom;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Controller{
 	
-	private String nick="volkano001";
+	final private String nick="volkano001";
 	private String password = "test";
 	
 	private String IP;
@@ -38,6 +42,8 @@ public class Controller{
 	private LinkedList<String> OPlist;
 	private LinkedList<String> NickList;
 
+	private boolean passiveClient = false; //determines if client active or passive
+
 	public Controller(String ip, int port){
 		IP = ip; this.port = port;
 	}
@@ -46,7 +52,7 @@ public class Controller{
 	Method for connecting to a dc hub
 
 	*/
-	public void connect() throws UnknownHostException,IOException{
+	public void connect() throws UnknownHostException,IOException, InterruptedException{
 		OPlist  = new LinkedList<String>();
 		NickList = new LinkedList<String>();
 	
@@ -60,25 +66,7 @@ public class Controller{
 		String[] items = response.split("\\|");
 		for (String item: items){
 			if (item.startsWith("$Lock")){
-				String[] values = item.split(" ");
-				String lock = values[1];
-				System.err.println("Lock value is "+lock);
-				int len = lock.length();
-				
-				//computing the key and sending...
-				String key  = ""+(char)(lock.charAt(0) ^ lock.charAt(len-1) ^ lock.charAt(len-2) ^ 5);
-				for (int i = 1; i < len; i++){
-					key += lock.charAt(i) ^ lock.charAt(i-1);
-				}
-				char[] newchars = new char[len];
-				for (int i = 0; i < len; i++){
-					char x = (char)((key.charAt(i) >> 4) & 0x0F0F0F0F);
-					char y = (char)((key.charAt(i) & 0x0F0F0F0F) << 4);
-					newchars[i] = (char)(x | y);
-				}
-				key = String.valueOf(newchars);
-				System.err.println("Sending "+key);
-				sendData("$Key "+key+"|");
+				getLockSendKey(item);
 			}
 		}
 
@@ -127,7 +115,100 @@ public class Controller{
 		//getting the file lists of all connected users
 		for (String user:NickList){
 			if (!user.equals(nick) && !OPlist.contains(user)){
-				System.out.println("Get file list of "+user);
+				if (passiveClient){
+					//passive client
+				}
+				else{
+					//active client
+					ServerSocket s = new ServerSocket(0);
+					int clientport = s.getLocalPort();
+					String clientip = "127.0.0.1";
+					sendData("$ConnectToMe "+user+" "+clientip+":"+clientport+"|");
+					String connectclientResponse = getResponse();
+					final Socket clientSocket = s.accept();
+					s.close();
+					//handling client connection
+					Thread t = new Thread(){
+						InputStream in;
+						OutputStream out;
+						String mynick = nick;
+						public void run(){
+							try{
+								
+								LinkedList<String> vals = new LinkedList<String>();
+								in = clientSocket.getInputStream();
+								out = clientSocket.getOutputStream();
+								//retrieve other client's nick
+								System.err.println("getting other clients name..");
+								String clientnick = getResponse();
+								for (String val:clientnick.split("\\|")){
+									vals.add(val);
+								}
+								//send other client my nick
+								sendData("$MyNick "+nick+"|");
+								sendData("$Lock EXTENDEDPROTOCOL"+getKey()+" Pk=Volkano"+version+"ABCABC|");
+								String lockclient = getResponse();
+								for (String val:lockclient.split("\\|")){
+									vals.add(val);
+								}
+								System.err.println("recived lock from client"+lockclient);
+								for (String lnitem:vals){
+									if (lnitem.startsWith("$Lock")){
+										System.err.println("other client's lock is "+lnitem);
+										getLockSendKey(lnitem);
+									}
+								}
+								//sending supports
+								sendData("$Supports MiniSlots XmlBZList ADCGet TTHL TTHF ZLIG|");
+								sendData("$Direction Download "+randInt(1,32767)+"|");
+								//retrive key response, etc
+								String keyresponse = getResponse();
+								System.err.println("respose for direction download is "+keyresponse);
+								sendData("$ADCGET file files.xml.bz2 0 -1 ZL1|");
+								System.err.println("Retrieving file..");
+								Scanner input = new Scanner(in);
+								input.useDelimiter("\\|");
+								while(input.hasNextLine()){
+									System.err.println(input.nextLine());
+								}
+							}catch(Exception e){
+								//gracefully fail
+							}
+						}
+						private String getResponse() throws IOException{
+							try{
+								Thread.sleep(30000);
+							}catch(Exception e){
+								//silently fail
+							}
+							for (int i=0; i<2; ++i){
+								try{
+									return reallygetResponse();
+								}catch(Exception e){
+									//silently fail
+								}
+							}
+							return null;
+						}
+						private String reallygetResponse() throws Exception{
+							int available = in.available();
+							byte[] string = new byte[available];
+							int val = in.read(string);
+							if (val==-1){
+								throw new Exception("Eish");
+							}
+							String f = new String(string, "UTF-8");
+							return f;
+						}
+						private void sendData(String data) throws IOException{
+							out.write(data.getBytes(Charset.forName("UTF-8")));
+							out.flush();
+						}
+					};
+					t.start();
+					System.err.println("Other client connected...");
+					t.join();
+				}
 			}
 		}
 	}
@@ -170,5 +251,96 @@ public class Controller{
 	private void sendData(String data) throws IOException{
 		hubOutput.write(data.getBytes(Charset.forName("UTF-8")));
 		hubOutput.flush();
+	}
+	/*
+	
+	Method for getting lock and sending key
+
+	*/
+	private void getLockSendKey(String item) throws IOException{
+		String[] values = item.split(" ");
+		String lock = sanitizeLockKey(values[1],1);
+		System.err.println("Lock value is "+lock);
+		int len = lock.length();
+		
+		//computing the key and sending...
+		String key  = ""+(char)(lock.charAt(0) ^ lock.charAt(len-1) ^ lock.charAt(len-2) ^ 5);
+		for (int i = 1; i < len; i++){
+			key += lock.charAt(i) ^ lock.charAt(i-1);
+		}
+		char[] newchars = new char[len];
+		for (int i = 0; i < len; i++){
+			char x = (char)((key.charAt(i) >> 4) & 0x0F0F0F0F);
+			char y = (char)((key.charAt(i) & 0x0F0F0F0F) << 4);
+			newchars[i] = (char)(x | y);
+		}
+		key = sanitizeLockKey(String.valueOf(newchars),0);
+		System.err.println("Sending "+key);
+		sendData("$Key "+key+"|");
+	}
+	/*
+	
+	Method for cleaning up lock/key
+
+	mode indicates how to clean lock/key. 0 for replace chars with odd strings, 1 remove odd string for chars
+	*/
+	public String sanitizeLockKey(String lockkey,int mode){
+		
+		int len = lockkey.length();
+		if (len==0){		
+			String sanitizedLockkey = "";
+			for (int i=0; i<len; ++i){
+				int val = lockkey.charAt(i);
+				switch(val){
+					case 0:{
+						sanitizedLockkey+="/%DCN000%/";
+						break;}
+					case 5:{
+						sanitizedLockkey+="/%DCN005%/";
+						break;}
+					case 36:{
+						sanitizedLockkey+="/%DCN036%/";
+						break;}
+					case 96:{
+						sanitizedLockkey+="/%DCN096%/";
+						break;}
+					case 124:{
+						sanitizedLockkey+="/%DCN124%/";
+						break;}
+					case 126:{
+						sanitizedLockkey+="/%DCN126%/";
+						break;}
+					default:{
+						sanitizedLockkey+=""+lockkey.charAt(i);
+						break;}
+				}
+			}
+			return sanitizedLockkey;
+		}
+		else{
+			lockkey=lockkey.replaceAll("/%DCN000%/",Character.toChars(0));
+			lockkey=lockkey.replaceAll("/%DCN005%/",Character.toChars(5));
+			lockkey=lockkey.replaceAll("/%DCN036%/",Character.toChars(36));
+			lockkey=lockkey.replaceAll("/%DCN096%/",Character.toChars(96));
+			lockkey=lockkey.replaceAll("/%DCN124%/",Character.toChars(124));
+			lockkey=lockkey.replaceAll("/%DCN126%/",Character.toChars(126));
+		}
+	}
+	/*
+	Method for generating random key
+	
+	method stolen form http://stackoverflow.com/a/41156
+	*/
+	private SecureRandom random = new SecureRandom();
+	public String getKey() {
+		return new BigInteger(130, random).toString(18);
+	}
+	/*
+	
+	Method for generating integers between range
+	
+	*/
+	public int randInt(int min, int max){
+		return min + (int)(Math.random() * ((max - min) + 1));
 	}
 }
