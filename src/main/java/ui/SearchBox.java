613b78126e17b5a67d7af25a02c53993844109db
AdeebNqo <adeebnqo@gmail.com>
2014-04-14 20:15:04 +0200

    finished deafult searchbox -- with testing in driver

--- src/main/java/ui/SearchBox.java
+++ src/main/java/ui/SearchBox.java
@@ -1,24 +1,45 @@
 package ui;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
-import javax.swing.border.LineBorder;
-import javax.swing.border.Border;
-import java.awt.Color;
 import javax.swing.JTextField;
-import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
 
-public class SearchBox extends JPanel{
+public class SearchBox extends JPanel implements ActionListener, KeyListener{
 	private JTextField  searchbox;
 	public SearchBox(ImageIcon searchIcon){
-		setLayout(new FlowLayout());
-		JLabel lbl = new JLabel();
-		lbl.setIcon(searchIcon);
-		add(lbl);
-		searchbox = new JTextField();
-		Border roundedBorder = new LineBorder(Color.green, 5, true);
-		searchbox.setBorder(roundedBorder);
-		add(searchbox);
+		JTextField searchbox = new JTextField();
+		searchbox.setBorder(BorderFactory.createEmptyBorder());
+		setLayout(new BorderLayout(1,2));
+
+		JButton searchbutton = new JButton(searchIcon);
+		searchbutton.setBorder(BorderFactory.createEmptyBorder());
+
+		add(searchbox, BorderLayout.CENTER);
+		add(searchbutton, BorderLayout.EAST);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+		searchbox.addKeyListener(this);
+		searchbutton.addActionListener(this);
 	}
+	/*
+	Handling button presses
+	*/
+	public void actionPerformed(ActionEvent e){
+
+	}
+	/*
+	Handling input from the searchbox
+	*/
+	public void keyReleased(KeyEvent e){}
+	public void keyPressed(KeyEvent e){}
+	public void keyTyped(KeyEvent e){}
+	
 }
